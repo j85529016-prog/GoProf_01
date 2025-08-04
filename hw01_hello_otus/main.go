e952da80cd09bdfa13859bec80e9a0abc09ce231
DNSemin <dnsemin@inno.tech>
2025-08-04 17:48:14 +0300

    hw01_hello_otus

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse" //nolint:depguard
+)
+
 func main() {
-	// Place your code here.
+	inputText := "Hello, OTUS!"
+	outputText := reverse.String(inputText)
+	fmt.Println(outputText)
 }
